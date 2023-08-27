9a77a58ccecec0f1376aa8239ebea7d76dc6d547
Roman Repin <romarepin23rus@gmail.com>
2023-08-27 15:59:47 +0300

    [refactoring] add dependency injection to lion class

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,8 +5,12 @@ import java.util.List;
 public class Lion {
 
     boolean hasMane;
+    private final Feline feline;
 
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, Feline feline) throws Exception {
+
+        this.feline = feline;
+
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -15,8 +19,6 @@ public class Lion {
             throw new Exception("Используйте допустимые значения пола животного - самей или самка");
         }
     }
-
-    Feline feline = new Feline();
 
     public int getKittens() {
         return feline.getKittens();
